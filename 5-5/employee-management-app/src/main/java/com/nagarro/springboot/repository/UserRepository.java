827af4fbdf40f14298ef026ac118eee8039f15be
Sanjanajat <86455379+Sanjanajat@users.noreply.github.com>
2021-06-29 11:57:03 +0530

    Add files via upload

---
+++ 5-5/employee-management-app/src/main/java/com/nagarro/springboot/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.nagarro.springboot.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nagarro.springboot.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+	User findByEmail(String email);
+
+}
